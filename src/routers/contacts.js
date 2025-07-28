67705f84662329f69c9720b48d49ac4d722595c5
Chekan <ky.chekan@gmail.com>
2025-07-28 15:36:43 +0300

    step 2

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -3,8 +3,9 @@ import {
   getContactsController,
   getContactByIdController,
 } from '../controllers/contacts.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 export const contactsRouter = express.Router();
 
-contactsRouter.get('/', getContactsController);
-contactsRouter.get('/:contactId', getContactByIdController);
+contactsRouter.get('/', ctrlWrapper(getContactsController));
+contactsRouter.get('/:contactId', ctrlWrapper(getContactByIdController));
