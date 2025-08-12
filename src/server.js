a8b7e02151bf3a0433bee823e4a25971418a28da
Chekan <ky.chekan@gmail.com>
2025-08-12 22:01:25 +0300

    changes from comments

--- src/server.js
+++ src/server.js
@@ -9,27 +9,30 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import errorHandler from './middlewares/errorHandler.js';
 import { authRouter } from './routers/auth.js';
 import { authenticate } from './middlewares/auth.js';
+
 import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import fs from 'fs';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const swaggerDocument = YAML.load(
-  path.join(__dirname, '..', 'docs', 'openapi.yaml'),
-);
+
+const swaggerFilePath = path.join(__dirname, '..', 'docs', 'swagger.json');
+const swaggerDocument = JSON.parse(fs.readFileSync(swaggerFilePath, 'utf-8'));
 
 export const setupServer = () => {
   const app = express();
 
   app.use(express.json());
   app.use(cors());
+  app.use(cookieParser());
+
+  app.use('/docs', express.static(path.join(__dirname, '..', 'docs')));
 
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-  app.use(cookieParser());
 
   app.use(
     pino({
@@ -39,7 +42,6 @@ export const setupServer = () => {
     }),
   );
 
-  // app.use('/contacts', contactsRouter);
   app.use('/auth', authRouter);
   app.use('/contacts', authenticate, contactsRouter);
 
